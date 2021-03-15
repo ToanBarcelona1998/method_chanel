142a408ee8cd488d5c1a4282c3a817e0aa5bdffd
ToanBarcelona1998 <77067868+ToanBarcelona1998@users.noreply.github.com>
2021-03-15 17:19:59 +0700

    ok
    ok

--- android/app/src/main/java/com/test/method_chanel/MainActivity.java
+++ android/app/src/main/java/com/test/method_chanel/MainActivity.java
@@ -1,6 +1,59 @@
 package com.test.method_chanel;
 
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+
 
 public class MainActivity extends FlutterActivity {
+    private static final String CHANNEL="call.demo";
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                        (call, result) -> {
+                            // Note: this method is invoked on the main thread.
+                            if (call.method.equals("getBatteryLevel")) {
+                                int batteryLevel = getBatteryLevel();
+                                ArrayList<String> arr= new ArrayList();
+                                arr.add("Nguyen Van Toan");
+                                arr.add("Nguyen Van Tien");
+                                arr.add("Cao Anh Dung");
+
+                                if (arr.size()>0) {
+                                    result.success(arr);
+                                } else {
+                                    result.error("UNAVAILABLE", "Battery level not available.", "Load data");
+                                }
+                            } else {
+                                result.notImplemented();
+                            }
+                        }
+                );
+    }
+    private int getBatteryLevel() {
+        int batteryLevel = -1;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+        } else {
+            Intent intent = new ContextWrapper(getApplicationContext()).
+                    registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+            batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) /
+                    intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        }
+
+        return batteryLevel;
+    }
 }
